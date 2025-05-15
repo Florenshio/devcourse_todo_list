26d7f46b0ef95aa1589b0f6abf47d5e980dbc9b9
Florenshio <florenshio95@gmail.com>
2025-05-15 19:32:41 +0900

    Feat: swagger, controller added to main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { TeamsController } from './teams/teams.controller';
+import { TasksController } from './tasks/tasks.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AuthModule],
+  controllers: [
+    AppController,
+    UsersController,
+    TeamsController,
+    TasksController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
