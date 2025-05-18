40f344226714dd62f38f6e29d109b42155a090ef
Florenshio <florenshio95@gmail.com>
2025-05-18 23:47:17 +0900

    Fix: Add TypeORM module and add `users` module, then delete the `users` controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,27 @@ import { UsersController } from './users/users.controller';
 import { TeamsController } from './teams/teams.controller';
 import { TasksController } from './tasks/tasks.controller';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/user/user.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot(), // .env 파일 로드
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+      synchronize: true, // 개발 환경에서만 true로 설정
+    }),
+    UsersModule,
+    AuthModule],
   controllers: [
     AppController,
-    UsersController,
     TeamsController,
     TasksController,
   ],
