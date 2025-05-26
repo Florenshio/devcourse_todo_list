e5cac9b1e0e2b9eebac2dfee3db2f9581e89df39
Florenshio <florenshio95@gmail.com>
2025-05-26 11:38:29 +0900

    Comment: Remove useless comments

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -19,10 +19,6 @@ import { ConfigService } from '@nestjs/config';
         signOptions: { expiresIn: '1h' },
       }),
     })
-    // JwtModule.register({
-    //   secret: process.env.JWT_SECRET ?? 'ㅁㄴㅇㄹ', // 환경 변수 사용 권장
-    //   signOptions: { expiresIn: '1h' },
-    // }),
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy, JwtAuthGuard],
